75cdd2155b2f07808e4f12efdcde99eb5620093f
mgremek <k.pawlak666@gmail.com>
2019-03-17 17:08:10 +0100

    nowy projekt asp .net mvc

---
+++ WypozyczalniaKsiazek.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WypozyczalniaKsiazek.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
