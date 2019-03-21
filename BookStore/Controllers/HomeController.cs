166d467289262625975a078d60bd24b80872cb96
mgremek <k.pawlak666@gmail.com>
2019-03-21 18:09:17 +0100

    dodany connstring do bazy na lokalnym serwerku.

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BookStore.DAL;
+using BookStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,12 @@ namespace BookStore.Controllers
 {
     public class HomeController : Controller
     {
+        private BooksContext booksContext = new BooksContext();
         // GET: Home
         public ActionResult Index()
         {
+            Category category = new Category();
+            booksContext.Authors.Add(new Author());
             return View();
         }
     }
